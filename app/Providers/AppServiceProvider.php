981daa34dd98bf0b6a1b3636860ab5a61413d7a0
鶴田和眞 <k.tsuruta.1108@gmail.com>
2020-11-13 00:46:03 +0900

    Modify forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,8 +38,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (request()->isSecure()) {
-            \URL::forceScheme('https');
+        if ($this->app->environment() == 'production') {
+            URL::forceSchema('https');
         }
     }
 }
